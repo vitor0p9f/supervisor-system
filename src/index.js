73424cd3440eadbed68d3ef09096c53247278069
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-09-08 00:16:50 +0000

    build(Database): connect and sync with the database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import routes from "./routes.js";
+import database from "../database/connection.js";
 
 const server = express();
 const port = process.env.PORT || 4000;
+
+try {
+  await database.authenticate();
+
+  await database.sync();
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
 server.use(routes);
 
