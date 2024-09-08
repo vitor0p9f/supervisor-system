6aa48951f73924c41e8633f58cb36fe025f27be3
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-09-08 00:49:38 +0000

    feat(Express): add esp32 route
    Get the route param and save it in the database.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import express from "express";
+import Record from "../database/models/record.js";
 
 const router = express.Router();
 
-router.get("/", (request, response) => {
-  response.send("Funciona");
+router.get("/esp32/:value", async (request, response) => {
+
+  const record = await Record.create({ value: request.params.value });
+
+  response.status(200).send("OK");
 });
 
 export default router;
