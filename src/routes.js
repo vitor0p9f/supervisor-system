5299f4a5dec2e017ecaa7b94d8b448946e5536be
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-09-15 19:04:23 +0000

    feat(Routes): add the frontend daily route
    The route returns all database records that belong to the day of the
    informed date.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,29 @@
 import express from "express";
 import Record from "../database/models/record.js";
+import { Op } from "sequelize";
+import moment from "moment-timezone";
 
 const router = express.Router();
 
 router.get("/esp32/:value", async (request, response) => {
-
   const record = await Record.create({ value: request.params.value });
 
-  response.status(200).send("OK");
+  response.status(200).send(record);
+});
+
+router.get("/frontend/daily/:date", async (request, response) => {
+  const records = await Record.findAll({
+    where: {
+      createdAt: {
+        [Op.between]: [
+          moment.tz(request.params.date, "America/Sao_Paulo").startOf("day"),
+          moment.tz(request.params.date, "America/Sao_Paulo").endOf("day"),
+        ],
+      },
+    },
+  });
+
+  response.status(200).json(records);
 });
 
 export default router;
