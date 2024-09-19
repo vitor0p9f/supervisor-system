2671f5b1247676c0fd7fca837384ecc70faf8d67
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-09-19 19:39:28 +0000

    feat(Routes): update the daily route
    Order the records by hour.
    
    Add a JSON response with an array of tuples with the value and the hour.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import Record from "../database/models/record.js";
-import { Op } from "sequelize";
+import { Op, fn, literal, col } from "sequelize";
 import moment from "moment-timezone";
 
 const router = express.Router();
+
+const timeZone = "America/Sao_Paulo";
 
 router.get("/esp32/:value", async (request, response) => {
   const record = await Record.create({ value: request.params.value });
@@ -13,17 +15,26 @@ router.get("/esp32/:value", async (request, response) => {
 
 router.get("/frontend/daily/:date", async (request, response) => {
   const records = await Record.findAll({
+    attributes: ["value", "createdAt"],
+    order: [[fn("EXTRACT", literal('HOUR FROM "createdAt"')), "ASC"]],
     where: {
       createdAt: {
         [Op.between]: [
-          moment.tz(request.params.date, "America/Sao_Paulo").startOf("day"),
-          moment.tz(request.params.date, "America/Sao_Paulo").endOf("day"),
+          moment.tz(request.params.date, timeZone).startOf("day"),
+          moment.tz(request.params.date, timeZone).endOf("day"),
         ],
       },
     },
   });
 
-  response.status(200).json(records);
+  response.status(200).json(
+    records.map((record) => ({
+      value: record.dataValues.value,
+      hour: parseInt(
+        moment.tz(record.dataValues.createdAt, timeZone).format("HH"),
+      ),
+    })),
+  );
 });
 
 router.get("/frontend/weekly/:date", async (request, response) => {
