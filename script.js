327d0afdd8f13d6a45be969c5a6da29d7f992982
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-09-21 20:32:32 +0000

    feat(Scripts): update the script.js file
    Add the case to update the graph when the user selects the weekly
    visualization.

--- script.js
+++ script.js
@@ -11,23 +11,39 @@ buttons.forEach((button) => {
 
     button.classList.add("active");
 
+    const date = moment().tz("America/Sao_Paulo").format("YYYY-MM-DD");
+    let response = null;
+    let values = null;
+
     switch (button.id) {
       case "daily":
-        const date = moment().tz("America/Sao_Paulo").format("YYYY-MM-DD");
-
-        const response = await fetch(`http://localhost:4000/frontend/daily/${date}`, {
+        response = await fetch(`http://localhost:4000/frontend/daily/${date}`, {
           method: 'GET',
           headers: {
             'Accept': 'application/json',
           },
         });
 
-        const values = await response.json();
+        values = await response.json();
 
         chart.destroy();
 
         chart = new Chart(canvas, generateDailyChartConfig(values));
+        break;
 
+      case "weekly":
+        response = await fetch(`http://localhost:4000/frontend/weekly/${date}`, {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+          },
+        });
+
+        values = await response.json();
+
+        chart.destroy();
+
+        chart = new Chart(canvas, generateWeeklyGraphConfig(values));
         break;
 
       default:
