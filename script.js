2f74be83a951a93a2772177a96fed8354b2b06ba
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-09-21 18:27:01 +0000

    feat(Script): update the script.js file
    Add the function that updates the graph each time the user clicks the visualization button.

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const buttons = document.querySelectorAll("button");
+const canvas = document.querySelector("#chart");
+
+let chart = initialGraph();
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    buttons.forEach(button => {
-      button.classList=""
-    })
+  button.addEventListener("click", async () => {
+    buttons.forEach((button) => {
+      button.classList = "";
+    });
 
-    button.classList.add("active")
+    button.classList.add("active");
+
+    switch (button.id) {
+      case "daily":
+        const date = moment().tz("America/Sao_Paulo").format("YYYY-MM-DD");
+
+        const response = await fetch(`http://localhost:4000/frontend/daily/${date}`, {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+          },
+        });
+
+        const values = await response.json();
+
+        chart.destroy();
+
+        chart = new Chart(canvas, generateDailyChartConfig(values));
+
+        break;
+
+      default:
+        break;
+    }
   });
 });
